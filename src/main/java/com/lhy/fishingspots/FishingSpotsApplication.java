efc25fec7eb54bf7054870843cb0f55246d51d69
liuheyong <dcr_liuhyk@dcits.com>
2025-12-27 16:46:02 +0800

    first commit

---
+++ src/main/java/com/lhy/fishingspots/FishingSpotsApplication.java
@@ -0,0 +1,13 @@
+package com.lhy.fishingspots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingSpotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingSpotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhy/fishingspots/FishingSpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhy.fishingspots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingSpotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
